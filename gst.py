44dfbb2547771632922a5f70df0c82e569b375fe
Raymond Choo Juan Yong <volvagia356@gmail.com>
2015-04-04 19:52:27 +0800

    Cut down on FWDC emulation while staying compatible

--- gst.py
+++ gst.py
@@ -5,23 +5,15 @@ from json import loads
 class FWDC(requests.Session):
     def __init__(self, *args, **kwargs):
         self.fwdc_data = {}
-        self.fwdc_data['FAST_CLIENT_WINDOW__'] = "FWDC.WND-0000-0000-0000"
-        self.fwdc_data['FAST_CLIENT_AJAX_ID__'] = 0
         super().__init__(*args, **kwargs)
     
-    def before_request(self):
-        self.fwdc_data['FAST_CLIENT_WHEN__'] = str(int(time()*1000))
-        self.fwdc_data['FAST_CLIENT_AJAX_ID__'] += 1
-
     def after_request(self, response):
         try:
             self.fwdc_data['FAST_VERLAST__'] = response.headers['Fast-Ver-Last']
-            self.fwdc_data['FAST_VERLAST_SOURCE__'] = response.headers['Fast-Ver-Source']
         except KeyError:
             pass
 
     def get(self, *args, **kwargs):
-        self.before_request()
         if "params" not in kwargs: kwargs['params'] = {}
         kwargs['params'].update(self.fwdc_data)
         r = super().get(*args, **kwargs)
@@ -29,7 +21,6 @@ class FWDC(requests.Session):
         return r
     
     def post(self, *args, **kwargs):
-        self.before_request()
         if "data" not in kwargs: kwargs['data'] = {}
         kwargs['data'].update(self.fwdc_data)
         r = super().post(*args, **kwargs)
